e59df8b05cf342a2ca026ce5da598ec53112dd37
Arthur Hartwig Carlsson <arthur@kiron.net>
2022-07-14 15:48:47 +0000

    Adds source files to compilation
    Still missing asset header files though...

--- oot-sys/build.rs
+++ oot-sys/build.rs
@@ -1,9 +1,23 @@
 use cc;
+use glob;
 
 fn main() {
-    /*
-    cc::Build::new()
-        .file("hhsd.c")
-        .compile("oot");
-    */
+    let mut build = cc::Build::new();
+
+    build
+        .include("oot/src")
+        .include("oot/include")
+        .flag("-m32")
+        .flag_if_supported("-Wno-unused-variable")
+        .flag_if_supported("-Wno-unknown-pragmas")
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-variable")
+        .flag_if_supported("-Wno-missing-braces")
+        .define("_LANGUAGE_C", None);
+
+    for file in glob::glob("oot/src/**/*.c").unwrap() {
+        build.file(file.unwrap());
+    }
+    
+    build.compile("oot");
 }
